047e512b8908635f4fe8eadcd74410b2b4adf740
Adan <ajpalacios.sist@gmail.com>
2019-10-19 13:40:51 -0500

    First commit

---
+++ src/main/java/com/procode/demospringboot/dao/PersonDao.java
@@ -0,0 +1,16 @@
+package com.procode.demospringboot.dao;
+
+import com.procode.demospringboot.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+}
---
+++ src/main/java/com/procode/demospringboot/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.procode.demospringboot.service;
+
+import com.procode.demospringboot.dao.PersonDao;
+import com.procode.demospringboot.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PersonService {
+
+    private final PersonDao persondao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao){
+        this.persondao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return persondao.insertPerson(person);
+    }
+
+}
